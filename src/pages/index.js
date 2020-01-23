ebf8bd2dce01ea649f4b7895381ce8df58425ead
Yuval Suede <yuval.suede@naturalint.com>
2020-01-23 12:19:25 +0200

    update website btn

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,27 @@
 import React from 'react'
-import { useEffect } from 'react';
-import { navigate } from 'gatsby';
+import { graphql } from 'gatsby'
+import WebsiteLayout from "../components/website-layout"
+import Hero from "../components/hero";
+import VendorList from "../components/vendor-list";
+import Article from "../components/article";
+import Iframe from 'react-iframe'
 
-export default () => {
-  useEffect(() => {
-    navigate('/makeup');
-  }, []);
-  return null;
-};
+export default ({ data }) => (
+
+    <WebsiteLayout>
+      <>
+        <Hero
+            topDescription={data.datoCmsCategory.topDescription}
+            topLongDescription={data.datoCmsCategory.topLongDescription}
+            fluid={data.datoCmsCategory.cover.fluid} />
+      </>
+    </WebsiteLayout>
+);
+
+
+export const query = graphql`
+  query CategoryQuery($slug: String!) {
+    
+  }
+`
 
